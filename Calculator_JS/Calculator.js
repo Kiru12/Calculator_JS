a0bbd68bf2cc3501ffc320ec9f91bd9bc80eaea6
Damian Wąsik <Damian1219@gmail.com>
2022-06-29 20:33:39 +0200

    Finally FrontEnd

---
+++ Calculator_JS/Calculator.js
@@ -0,0 +1,15 @@
+console.log("Javascript Calculator by Kiru https://github.com/Kiru12")
+
+const buttons = document.getElementById("button_resume");
+const textBox = document.getElementById("center-block");
+
+/*for (btn in buttons)
+{
+    
+}
+
+btn.onclick = function() {
+    textBox.value = "40";
+};
+*/
+
